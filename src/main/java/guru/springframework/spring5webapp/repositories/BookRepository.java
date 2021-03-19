956c7b07a30406f37b4d7177e566985353abf153
Iptamenos <chriskormaris@gmail.com>
2021-03-19 18:22:32 +0200

    - Add interface `AuthorRepository`. - Add interface `BookRepository`.

---
+++ src/main/java/guru/springframework/spring5webapp/repositories/BookRepository.java
@@ -0,0 +1,15 @@
+package guru.springframework.spring5webapp.repositories;
+/*
+ * Athens 2021
+ *
+ * Created on : 2021-03-19
+ * Author     : Christos Kormaris
+ */
+
+import guru.springframework.spring5webapp.domain.Author;
+import guru.springframework.spring5webapp.domain.Book;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BookRepository extends CrudRepository<Book, Author> {
+
+}
