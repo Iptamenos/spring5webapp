bf78c3bf3f33c2f13dd2058bf63bdf5891cd4c51
Iptamenos <chriskormaris@gmail.com>
2021-04-20 14:52:50 +0300

    - Add Java class `AuthorController`. - Add template file `list.html`.

---
+++ src/main/java/guru/springframework/spring5webapp/controllers/AuthorController.java
@@ -0,0 +1,31 @@
+package guru.springframework.spring5webapp.controllers;
+/*
+ * Athens 2021
+ *
+ * Created on : 2021-04-20
+ * Author     : Christos Kormaris
+ */
+
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+
+    private final AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model) {
+
+        model.addAttribute("authors", authorRepository.findAll());
+
+        return "authors/list";
+    }
+}
